e680a74571898ebc82aab316fbe9b645f64af336
Alexander Peter <alexander.peter@bzz.ch>
2025-08-26 11:08:46 +0200

    commit

---
+++ src/main/java/ch/bzz/Book.java
@@ -0,0 +1,27 @@
+package ch.bzz;
+
+public class Book {
+        private final int id;
+        private final String isbn;
+        private final String title;
+        private final String author;
+        private final int year;
+
+        public Book(int id, String isbn, String title, String author, int year) {
+            this.id = id;
+            this.isbn = isbn;
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String toString() {
+            return "ID: " + id + ", ISBN: " + isbn + ", Title: " + title + ", Author: " + author + ", Year: " + year;
+        }
+    }
+
--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,27 @@
 package ch.bzz;
+import java.util.Scanner;
+
 
 public class LibraryAppMain {
 
+    public static final Book BOOK1 = new Book(1, "978-3-8362-9544-4", "Java ist auch eine Insel", "Christian Ullenboom", 2023);
+    public static final Book BOOK2 = new Book(2, "978-3-658-43573-8", "Grundkurs Java", "Dietmar Abts", 2024);
+
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner scanning = new Scanner(System.in);
+        System.out.println(BOOK1.getTitle());
+        while (true) {
+            String userinput = scanning.nextLine();
+            if (userinput.equals("quit")){
+                break;
+            } else if (userinput.equals("help")) {
+                System.out.println("quit for quit, help for help");
+            } else {
+                System.out.println("command not found: " + userinput);
+            }
+
+
+        }
     }
+
 }
